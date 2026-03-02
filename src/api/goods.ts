463d4cc55e76f269ae8e3b7bda0e05d51da28ab1
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2026-03-02 22:25:29 +0200

    add response.ok

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -1,10 +1,17 @@
+import { error } from 'console';
 import { Good } from '../types/Good';
 
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL).then(response => {
+    if (!response.ok) {
+      throw new Error(`Server error: ${response.status}`);
+    }
+
+    return response.json();
+  });
 }
 
 export const get5First = () => {
